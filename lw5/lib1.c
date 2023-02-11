1617ef708254f7a13404879527ea17b1b856ba24
Nikita Lokhmatov <nikitalochmatov@gmail.com>
2023-02-11 11:04:41 +0300

    Add files via upload

--- lw5/lib1.c
+++ lw5/lib1.c
@@ -15,13 +15,13 @@ int PrimeCount(int A, int B) {
         int flag = 0;
 
         for (int j = 2; j < i; j++) {
-            if (i % j != 0) {
+            if (i % j == 0) {
                 flag = 1;
                 break;
             }
         }
 
-        if (flag == 1) {
+        if (flag == 0) {
             count++;
         }
     }
